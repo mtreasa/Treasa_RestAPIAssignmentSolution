93adc1283052db2fcc95cfd62f8903b26fdf9789
mtreasa <97844122+mtreasa@users.noreply.github.com>
2022-07-22 09:40:44 +0530

    Update Data.sql

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -1,3 +1,6 @@
 insert into roles (name) values ('ADMIN');
 INSERT INTO USERS VALUES (1,'$2a$12$g7LIFT0QVdzvRG018STucuE4XvfSWX55Vd9p5EbdMFMuMTSAPbBji','treasam');
 insert into users_roles (user_id, role_id) values (1, 1);
+insert into roles (name) values ('USER');
+INSERT INTO USERS VALUES (2,'$2a$12$xpB5OMgJhTrhDKnrlZY5UeGd0yb/jG6RpvuLvcPn2epzPxDKp2liO','treasa');
+insert into users_roles (user_id, role_id) values (2, 2);
